9d2025830eabb053f4ec482be7e708baef4a9b8e
John Doe <gustavo.sena0@gmail.comGustavo Barbosa Sena>
2021-10-04 10:40:00 -0300

    get route venda feito

--- routes/venda.route.js
+++ routes/venda.route.js
@@ -1,2 +1,17 @@
 const {Router} = require('express')
 const Venda = require('../models/Venda.model')
+
+const route = Router()
+
+route.get('/vendas', async (req,res) => {
+
+    try {
+        const vendas = await Venda.find()
+        res.status(200).json(vendas)
+    } catch (error) {
+        res.status(500).json({msg: "Error"})
+    }
+
+})
+
+module.exports = route
