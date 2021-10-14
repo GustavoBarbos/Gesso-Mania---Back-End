b9933fcb18f079056c3b07826af62c3da9784770
Tiago R <tiagobiagirebelato@gmail.com>
2021-10-14 15:19:28 -0300

    Model Fechamento criado

---
+++ models/Fechmaneto.mode.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require("mongoose");
+
+const FechamentoSchema = new Schema({
+  data: {type:Date, required},
+  valor_total_vendas_do_mes: Number,
+  salarios_colaboradores: Number,
+  aluguel: Number,
+  valor_total_compras_do_mes: Number,
+  DespesasGerais: [
+    { nome: String, valor: Number, descrição: String },
+  ],
+  despesas_gerais_valor: Number,
+  despesas_totais: Number,
+  resultado: Number,
+});
+
+module.exports = model("Fechamento", FechamentoSchema);
